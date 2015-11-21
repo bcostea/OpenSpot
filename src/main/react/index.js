17cd1564fb4ca11d883f2210b257c5db2bf3bcad
Constantin Dumitrescu <dum.constantin@gmail.com>
2015-11-21 22:32:28 +0200

    Add points on map.

--- src/main/react/index.js
+++ src/main/react/index.js
@@ -13,7 +13,23 @@ const mapProps = {
     width: '100%'
   }
 };
-
+function makeMarker (item) {
+   return {
+     position: {
+       lat: item.position[0] + Math.random() / 5,
+       lng: item.position[1] + Math.random() / 5
+     },
+     key: item.id + Math.random(),
+     defaultAnimation: 2,
+     icon: {
+       path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+       fillOpacity: 0.8,
+       scale: 1,
+       strokeColor: item.status === 'FREE' ? 'green' : 'red',
+       strokeWeight: 14
+     }
+   }
+}
 var App = React.createClass({
   getInitialState: function () {
     return {
@@ -28,27 +44,17 @@ var App = React.createClass({
     var that = this;
     let stompClient = Stomp.over(new SockJS('/api/public/websocket/spots/all'));
     this.stompClient = stompClient;
+    console.log(that);
 
     stompClient.connect({}, function(frame) {
         stompClient.subscribe('/topic/parkingSpots', function(parkingSpots){
           let markers = JSON.parse(parkingSpots.body)
-          markers = markers.map(function (item) {
-             return {
-               position: {
-                 lat: item.position[0],
-                 lng: item.position[1]
-               },
-               key: item.id,
-               defaultAnimation: 2,
-               icon: {
-                 path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-                 fillOpacity: 0.8,
-                 scale: 1,
-                 strokeColor: item.status === 'FREE' ? 'green' : 'red',
-                 strokeWeight: 14
-               }
-             }
-          });
+          if (typeof markers !== 'array') {
+            console.log(makeMarker(markers));
+            markers = that.state.markers.concat([makeMarker(markers)]);
+          } else {
+            markers = markers.map(makeMarker);
+          }
           that.setState({ markers: markers });
         });
 
