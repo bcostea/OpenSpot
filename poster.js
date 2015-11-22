51d988d44d67638f950915c4cf55235c67a044e2
Bogdan Costea <bogdan.costea@siveco.ro>
2015-11-22 03:09:50 +0200

    lot

--- poster.js
+++ poster.js
@@ -1,11 +1,22 @@
 //Load the request module
 var request = require('request');
 
+var spots =
+['5650e704715c84e8b4a547d8',
+'5650e704715c84e8b4a547d9',
+'5650e704715c84e8b4a547da',
+'5650e704715c84e8b4a547db',
+'5650e704715c84e8b4a547dc'];
+
+var client = function(){
+    var spotNum =  Math.floor(Math.random() * 4);
+    var spot = spots[spotNum];
+    var status = Math.floor(Math.random() * 10) > 5? true:false;
 
     request({
-        url: 'http://localhost:8080/api/public/spots/565113787c276a810296376b/update',
+        url: 'http://localhost:8080/api/public/spots/' + spot + '/update',
         method: 'POST',
-        json: {id: '565113787c276a810296376b', free:false}
+        json: {id: spot, free:status}
     }, function(error, response, body){
         if(error) {
             console.log(error);
@@ -13,3 +24,6 @@ var request = require('request');
             console.log(response.statusCode, body);
     }
     });
+}
+
+setInterval(client, 1000);
