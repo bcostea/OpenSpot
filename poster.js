bf0aa577c5450a4ed6a160ed4432cd113aa44d30
Constantin Dumitrescu <dum.constantin@gmail.com>
2015-11-22 06:17:26 +0200

    Read spots from db when running poster.js

--- poster.js
+++ poster.js
@@ -1,18 +1,35 @@
 //Load the request module
 var request = require('request');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log('done');
+});
 
-var spots =
-["56511e6c715cb5a4b236bc64",
-"56511e6c715cb5a4b236bc65",
-"56511e6c715cb5a4b236bc66",
-"56511e6c715cb5a4b236bc67",
-"56511e6c715cb5a4b236bc68",
-"56511e6c715cb5a4b236bc69",
-"56511e6c715cb5a4b236bc6a",
-"56511e6c715cb5a4b236bc6b"];
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost/test';
+var spots = [];
+
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('parking_spots').find().each(function (err, doc) {
+      assert.equal(err, null);
+      if (doc) {
+        spots.push(doc._id);
+      } else {
+        console.log(spots);
+        startUpdates();
+      }
+    });
+});
+
 
 var client = function(){
-    var spotNum =  Math.floor(Math.random() * 4);
+    var spotNum =  Math.floor(Math.random() * spots.length - 1);
     var spot = spots[spotNum];
     var status = Math.floor(Math.random() * 10) > 5? true:false;
 
@@ -29,4 +46,6 @@ var client = function(){
     });
 }
 
-setInterval(client, 1000);
+function startUpdates() {
+  setInterval(client, 100);
+}
